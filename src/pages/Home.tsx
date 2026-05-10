4df6f4ada2bf2d24ce2f8e8b747b482af35f3062
200-hassan <md.arhassan@gmail.com>
2026-05-10 19:23:11 +0600

    Create browse categories section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import Features from "../components/Home/Features"
 import Hero from "../components/Home/Hero"
+import HomeCategories from "../components/Home/HomeCategories"
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <Hero />
         <Features />
+        <HomeCategories />
     </div>
   )
 }
