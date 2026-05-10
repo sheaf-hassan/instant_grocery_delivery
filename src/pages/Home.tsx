a8ca092aafd5eaaa4f2782668f6e34c4a452b92e
200-hassan <md.arhassan@gmail.com>
2026-05-10 23:09:47 +0600

    create product cart and popular products section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Features from "../components/Home/Features"
 import Hero from "../components/Home/Hero"
 import HomeCategories from "../components/Home/HomeCategories"
+import PopularProducts from "../components/Home/PopularProducts"
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
         <Hero />
         <Features />
         <HomeCategories />
+        <PopularProducts />
     </div>
   )
 }
