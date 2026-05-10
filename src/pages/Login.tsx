451c8fd30657aa79705f5646ba63abcc55778174
200-hassan <md.arhassan@gmail.com>
2026-05-10 14:18:37 +0600

    create sign up and sign in form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { heroSectionData } from "../assets/assets";
+import { Link } from "react-router-dom";
+import { BikeIcon, Loader2Icon, LockIcon, MailIcon, UserIcon } from "lucide-react";
 
 const login = () => {
   const [isLoginState, setIsLoginState] = useState(true);
@@ -28,6 +30,79 @@ const login = () => {
         </div>
 
         {/* Right Side */}
+        <div className="flex-1 flex-center px-4 py-12 bg-app-cream">
+            <div className="w-full max-w-md">
+                {/* Form header message */}
+                <div className="text-center mb-8">
+                    <Link to="/" className="inline-flex items-center gap-2 mb-6">
+                        <BikeIcon className="size-8 text-app-green"/>
+                        <span className="text-2xl font-semibold text-app-green">Instant Grocery</span>
+                    </Link>
+                    <h1 className="text-2xl font-semibold text-app-green mb-2">
+                        {isLoginState ? "Sign in to your account" : "Sign up for a new account"}
+                    </h1>
+                    <p className="text-sm text-app-text-light">
+                        {isLoginState ? "Don't have an account" : "Already have an account"} 
+                        <button onClick={() => setIsLoginState(!isLoginState)}
+                            className="text-orange-500 ml-1 font-semibold hover:text-orange-600 transition-colors"
+                        >
+                            {isLoginState ? "Create one" : "Sign in"}
+                        </button>
+                    </p>
+                </div>
+
+                {/* Login/Register form */}
+                <form onSubmit={handleSubmit} className="space-y-5">
+                    {!isLoginState && (
+                        <label className="text-sm flex flex-col gap-1">
+                            Name
+                            <div className="relative">
+                                <UserIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light"/>
+                                <input 
+                                type="text"
+                                value={name}
+                                onChange={(e) =>setName(e.target.value)}
+                                required
+                                placeholder="Your name"
+                                className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                                />
+                            </div>
+                        </label>
+                    )}
+                    <label className="text-sm flex flex-col gap-1">
+                            Email Address
+                            <div className="relative">
+                                <MailIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light"/>
+                                <input 
+                                type="email"
+                                value={email}
+                                onChange={(e) =>setEmail(e.target.value)}
+                                required
+                                placeholder="you@example.com"
+                                className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                                />
+                            </div>
+                    </label>
+                    <label className="text-sm flex flex-col gap-1">
+                            Password
+                            <div className="relative">
+                                <LockIcon className="absolute left-3.5 top-1/2 -translate-y-1/2 size-4 text-app-text-light"/>
+                                <input 
+                                type="password"
+                                value={password}
+                                onChange={(e) =>setPassword(e.target.value)}
+                                required
+                                placeholder="........"
+                                className="w-full pl-11 pr-4 py-3 text-sm bg-white rounded-xl border not-focus:border-app-border transition-all"
+                                />
+                            </div>
+                        </label>
+                        <button type="submit" disabled={loading} className="flex-center w-full py-3 bg-green-950 text-white font-semibold rounded-xl hover:bg-green-900 transition-colors disabled:opacity-50">
+                            {loading ? <Loader2Icon className="animate-spin"/> : isLoginState ? "Sign In" : "Sign Up"}
+                        </button>
+                </form>
+            </div>
+        </div>
     </div>
   )
 }
