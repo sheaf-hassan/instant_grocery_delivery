4b9af9e5989c58c78af3f9ada6bdb93a9fb85b5c
200-hassan <md.arhassan@gmail.com>
2026-05-14 04:32:40 +0600

    create filter panel component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -5,6 +5,8 @@ import { categoriesData, dummyProducts } from "../assets/assets";
 import { ChevronDown, Home, SlidersHorizontal } from "lucide-react";
 import ProductCard from "../components/Home/ProductCard";
 import Loading from "../components/Loading";
+import FilterPanel from "../components/FilterPanel";
+
 
 
 
@@ -67,7 +69,7 @@ const Products = () => {
           {/* Sidebar - desktop */}
           <aside className="hidden lg:block w-64 shrink-0">
             <div className="bg-white  rounded-2xl p-4 sticky top-24">
-              <p>Filter</p>
+              <FilterPanel categories={categoriesData} category={category} organic={organic} minPrice={minPrice} maxPrice={maxPrice} updateFilter={updateFilter} clearFilters={clearFilters} hasFilters={hasFilters}/>
             </div>
           </aside>
 
