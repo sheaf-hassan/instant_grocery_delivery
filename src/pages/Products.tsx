1933ff6469cd8387e07f257b71d67afe91bd0ef9
200-hassan <md.arhassan@gmail.com>
2026-05-13 16:01:13 +0600

    create the functionality for products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom"
+import type { Product } from "../types";
+import { categoriesData, dummyProducts } from "../assets/assets";
+
 
 
 const Products = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [products, setProducts] = useState<Product[]>();
+  const[totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [mobileFiltersOpen, SetMobileFiltersOpen] = useState(false);
+
+  const category = searchParams.get("category") || "";
+  const organic = searchParams.get("organic") || "";
+  const sort = searchParams.get("sort") || "";
+  const page = Number(searchParams.get("page")) || 1;
+  const minPrice = searchParams.get("minPrice") || "";
+  const maxPrice = searchParams.get("maxPrice") || "";
+
+  const fetchProducts = async () =>{
+    setLoading(true)
+    setProducts(dummyProducts.filter((p)=> p.category === category || category === ""));
+    setLoading(false)
+  };
+
+  const updateFilter = (key: string, value: string)=>{
+    const newParams = new URLSearchParams(searchParams)
+    if(value){
+      newParams.set(key, value)
+    } else{
+      newParams.delete(key)
+    }
+    if(key !== "page"){
+      newParams.delete("page")
+    }
+    setSearchParams(newParams)
+  }
+
+  const clearFilters = ()=> setSearchParams({});
+
+  const activecategory = categoriesData.find((c)=> c.slug === category);
+  const hasFilters = category || organic || minPrice || maxPrice;
+
+  useEffect(()=>{
+    fetchProducts()
+  }, [category, organic, sort, page, minPrice, maxPrice])
+
   return (
-    <div>Products</div>
+    <div></div>
   )
 }
 
