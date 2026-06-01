9989783d1e56cc2b7811c7b1aaec19be398359dd
200-hassan <md.arhassan@gmail.com>
2026-06-01 12:58:36 +0600

    create the function  for user login

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -44,3 +44,35 @@ export const register = async (req:Request, res:Response) =>{
 
     res.status(201).json({user: userData, token})
 }
+
+
+// Login
+// POST /api/auth/login
+export const login = async (req:Request, res:Response) =>{
+    const {email, password} = req.body;
+
+    if(!email || !password){
+        return res.status(400).json({message: "Please provide email and password"})
+    }
+
+    const user = await prisma.user.findUnique({where: {email: email.toLowerCase()}, include: {addresses: true}});
+
+    if(!user){
+        return res.status(401).json({message: "Invalid email or password"})
+    }
+
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch){
+        return res.status(401).json({message: "Invalid email or password"});
+    }
+    
+
+    const token = generateToken(user.id)
+
+    const userData: any = {...user};
+    delete userData.password;
+    userData.isAdmin = getAdminStatus(userData.email)
+
+    res.json({user: userData, token})
+}
