c39f0563e1d852f9f252d256d2a739e67bd52a51
200-hassan <md.arhassan@gmail.com>
2026-06-04 11:39:48 +0600

    integrate nodemailer and inngest to the app

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,9 @@ import authRouter from "./routes/authRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import uploadRouter from "./routes/uploadRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
 
 const app = express();
 
@@ -22,6 +25,7 @@ app.use('/api/auth', authRouter)
 app.use('/api/products', productRouter)
 app.use('/api/upload', uploadRouter)
 app.use('/api/orders', orderRouter)
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction)=>{
