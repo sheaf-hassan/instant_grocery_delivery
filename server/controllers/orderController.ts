9d3ba3308dd0c4e4831756ec5382ab4fcfd01b32
200-hassan <md.arhassan@gmail.com>
2026-06-04 16:15:55 +0600

    vercel configuration for the clint and server

--- server/controllers/orderController.ts
+++ server/controllers/orderController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prisma.js";
+import { inngest } from "../inngest/index.js";
 
 
 // Create order
@@ -73,6 +74,13 @@ export const createOrder = async (req: Request, res: Response)=>{
             data: {stock: {decrement: item.quantity}}
         })
     }
+
+    // Send stock update events for each product in the order
+    for(const item of orderItems){
+        await inngest.send({name: "inventory/stock.updated", data: {productId: item.product}})
+    }
+
+    await inngest.send({name: "order/placed", data: {orderId: order.id}})
 }
 
 // Get user's orders
