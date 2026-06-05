2e943dddb4a3077018ce2469340e701dd7991376
200-hassan <md.arhassan@gmail.com>
2026-06-05 21:51:01 +0600

    create the delivery auth middleware

---
+++ server/routes/deliveryPartnerRoutes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getMyDeliveries, loginPartner } from '../controllers/deliveryPartnerController.js';
+
+
+const deliveryPartnerRouter = express.Router();
+
+deliveryPartnerRouter.post('/login', loginPartner)
+deliveryPartnerRouter.get('/my-deliveries', getMyDeliveries)
+
+
+
+export default deliveryPartnerRouter;
