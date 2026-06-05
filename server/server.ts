af3a17381f235987b34feddcea60be0f90513720
200-hassan <md.arhassan@gmail.com>
2026-06-05 22:07:07 +0600

    create the delivery partner routes and integrate the delivery api with the app

--- server/server.ts
+++ server/server.ts
@@ -9,6 +9,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import addressRouter from "./routes/addressRoutes.js";
 import adminRouter from "./routes/adminRoutes.js";
+import deliveryPartnerRouter from "./routes/deliveryPartnerRoutes.js";
 
 
 const app = express();
@@ -30,6 +31,7 @@ app.use('/api/orders', orderRouter);
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use('/api/addresses', addressRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/delivery', deliveryPartnerRouter);
 
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction)=>{
