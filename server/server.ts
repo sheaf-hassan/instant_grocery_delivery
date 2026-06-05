5181e0afb7dff9a5ee513597cbeaea7f9a835979
200-hassan <md.arhassan@gmail.com>
2026-06-05 14:44:23 +0600

    create the api for admin features

--- server/server.ts
+++ server/server.ts
@@ -8,6 +8,7 @@ import orderRouter from "./routes/orderRoutes.js";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import addressRouter from "./routes/addressRoutes.js";
+import adminRouter from "./routes/adminRoutes.js";
 
 
 const app = express();
@@ -28,6 +29,7 @@ app.use('/api/upload', uploadRouter);
 app.use('/api/orders', orderRouter);
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use('/api/addresses', addressRouter);
+app.use('/api/admin', adminRouter);
 
 // Error handling
 app.use((error: any, req: Request, res: Response, next: NextFunction)=>{
