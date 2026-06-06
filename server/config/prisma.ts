07b4f9499ff45d3e2d8aa0db958071a4fef11a50
200-hassan <md.arhassan@gmail.com>
2026-06-06 15:02:33 +0600

    create the seed file and complete the backend server

--- server/config/prisma.ts
+++ server/config/prisma.ts
@@ -1,6 +1,10 @@
 import 'dotenv/config'
 import { PrismaClient } from '../generated/prisma/client.js'
 import { PrismaNeon } from '@prisma/adapter-neon'
+import { neonConfig } from '@neondatabase/serverless'
+import ws from 'ws'
+
+neonConfig.webSocketConstructor = ws
 
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL!,
