c90a0bd02093c46c7a4f37e88cf4cb3a20c3b3f4
200-hassan <md.arhassan@gmail.com>
2026-06-07 05:28:57 +0600

    integrate fetching product details from the database

--- clint/src/pages/ProductPage.tsx
+++ clint/src/pages/ProductPage.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import type { Product } from "../types";
-import { dummyProducts } from "../assets/assets";
 import Loading from "../components/Loading";
 import { useCart } from "../context/CartContext";
 import {
@@ -16,6 +15,7 @@ import {
 } from "lucide-react";
 import DummyReviewsSection from "../assets/DummyReviewsSection";
 import ProductCard from "../components/Home/ProductCard";
+import api from "../config/api";
 
 const ProductPage = () => {
   const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
@@ -32,10 +32,14 @@ const ProductPage = () => {
     setLoading(true);
     setLocalQuantity(1);
     window.scrollTo(0, 0);
-    const product = dummyProducts.find((p) => p.id === id);
-    setProduct(product!);
-    setRelatedProducts(dummyProducts.filter((p) => p.id !== id));
-    setLoading(false);
+    
+    api.get(`/products/${id}`).then(({data})=>{
+      setProduct(data.product);
+      return api.get(`/products?category=${data.product.category}`)
+    }).then(({data})=>{
+      setRelatedProducts(data.products.filter((p: Product)=> p.id !== id))
+    }).catch(()=> navigate("/products")).finally(()=> setLoading(false))
+
   }, [id, navigate]);
 
   if (loading) return <Loading />;
