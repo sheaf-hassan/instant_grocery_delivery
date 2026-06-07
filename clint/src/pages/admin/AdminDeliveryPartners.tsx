e125279fd934e55c563f42b0190e679393803c32
200-hassan <md.arhassan@gmail.com>
2026-06-08 05:32:00 +0600

    integrate the adding of delivery partner at admin panel

--- clint/src/pages/admin/AdminDeliveryPartners.tsx
+++ clint/src/pages/admin/AdminDeliveryPartners.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { PlusIcon, XIcon, TruckIcon, PhoneIcon, MailIcon } from "lucide-react";
 import type { DeliveryPartner } from "../../types";
 import Loading from "../../components/Loading";
-import { dummyDeliveryPartnerData } from "../../assets/assets";
+import api from "../../config/api";
+import toast from "react-hot-toast";
+
 
 export default function AdminDeliveryPartners() {
     const [partners, setPartners] = useState<DeliveryPartner[]>([]);
@@ -12,8 +14,14 @@ export default function AdminDeliveryPartners() {
     const [form, setForm] = useState({ name: "", email: "", password: "", phone: "", vehicleType: "bike" });
 
     const fetchPartners = async () => {
-        setPartners(dummyDeliveryPartnerData as any)
-        setTimeout(() => setLoading(false), 1000)
+        try {
+            const { data } = await api.get("/admin/delivery-partners");
+            setPartners(data.partners)
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || "failed");
+        }finally{
+            setLoading(false)
+        }
     };
 
     useEffect(() => {
@@ -22,6 +30,18 @@ export default function AdminDeliveryPartners() {
 
     const handleSubmit = async (e: React.SubmitEvent) => {
         e.preventDefault();
+        setSaving(true)
+        try {
+            await api.post("/admin/delivery-partners", form);
+            toast.success("Partner onboarded successfully!");
+            setShowForm(false);
+            setForm({name: "", email: "", password: "", phone: "", vehicleType: "bike"});
+            fetchPartners();
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || "Failed");
+        }finally{
+            setSaving(false)
+        }
 
     };
 
