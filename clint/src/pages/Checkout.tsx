a434884130b05d0d78ec900172b680bc18bf9ec4
200-hassan <md.arhassan@gmail.com>
2026-06-07 16:29:56 +0600

    integrate the checkout and place order option

--- clint/src/pages/Checkout.tsx
+++ clint/src/pages/Checkout.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom"
 import { useCart } from "../context/CartContext";
-import { dummyAddressData } from "../assets/assets";
 import type { Address } from "../types";
 import { ArrowLeft, CheckIcon, ChevronRight, CreditCardIcon, MapPinIcon } from "lucide-react";
 import CheckoutAddress from "../components/Checkout/CheckoutAddress";
 import CheckoutPayment from "../components/Checkout/CheckoutPayment";
 import CheckoutReview from "../components/Checkout/CheckoutReview";
+import api from "../config/api";
+import toast from "react-hot-toast";
+import { useAuth } from "../context/AuthContext";
 
 
 const Checkout = () => {
@@ -14,8 +16,8 @@ const Checkout = () => {
   const navigate = useNavigate();
   const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
 
-  const {items, cartTotal} = useCart();
-  const {user} = {user: {addresses: dummyAddressData}};
+  const {items, cartTotal, clearCart} = useCart();
+  const {user} = useAuth();
 
   const [step, setStep] = useState("address");
   const [loading, setLoading] = useState(false);
@@ -46,7 +48,32 @@ const Checkout = () => {
 
   const handlePlaceOrder = async () => {
     setLoading(true)
-    navigate("/orders")
+    try {
+      const orderData = {
+        items: items.map((item)=>({
+          product: item.product.id,
+          quantity: item.quantity,
+        })),
+        shippingAddress: address,
+        paymentMethod
+      }
+
+      const { data } = await api.post('/orders', orderData)
+      console.log(data)
+
+      if(data.url){
+        window.location.href = data.url;
+        return;
+      }
+      clearCart()
+      toast.success("Order placed successfully!");
+      navigate(`/orders/${data.order.id}`)
+    } catch (error: any) {
+      toast.error(error.response?.data?.message || error.message);
+    }finally{
+      setLoading(false);
+      scrollTo(0, 0);
+    }
   }
   
   
